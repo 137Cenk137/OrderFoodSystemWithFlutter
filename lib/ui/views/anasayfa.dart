4f4a5bb59eace0e177cde82919bc1813fe251c87
Cenk Kıran <cenk33m@gmail.com>
2024-02-26 21:16:33 +0300

    bottomNavigationbar was added

--- lib/ui/views/anasayfa.dart
+++ lib/ui/views/anasayfa.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:yemek_sistemi/data/entity/yemekler.dart';
 import 'package:yemek_sistemi/ui/cubit/anasayfa_cubit.dart';
-import 'package:yemek_sistemi/ui/cubit/detail_page_cubit.dart';
-import 'package:yemek_sistemi/ui/views/detail_page.dart';
+import 'package:yemek_sistemi/ui/views/main_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -15,6 +13,8 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
 
 var tfSearch = TextEditingController();
+int secilenIndex = 0;
+var sayfalar = [const MainPage2(),const MainPage2(),const MainPage2(),const MainPage2()];
 
 
 @override
@@ -33,80 +33,22 @@ var tfSearch = TextEditingController();
           IconButton(onPressed: (){}, icon: Icon(Icons.search))
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
+      body: sayfalar[secilenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.house),label: "Anasayfa"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite),label: "Favori"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_basket_outlined),label: "hesap"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded),label: "Sepet")
+        ],
 
-            BlocBuilder<MainPageCubit,List<Food>>(builder:
-                (context,foodList) {
-              if(foodList.isNotEmpty){
-                return Expanded(
-                  child: GridView.builder(
-                      itemCount: foodList.length,
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  
-                      crossAxisCount: 2,childAspectRatio: 1 / 2
-                  ), itemBuilder: (context,index){
-                      var food = foodList[index];
-                      return
-                         Padding(
-                           padding: const EdgeInsets.all(10.0),
-                           child: GestureDetector(
-                             onLongPress: (){
-                               Navigator.push(context, MaterialPageRoute(builder: (context)=>DeatilPage(food: food))).then((v) {
-                                 context.read<MainPageCubit>().getAllFood();
-                               });
-                             },
-                             child: Card(
+        currentIndex: secilenIndex,
+        onTap: (index){
+          setState(() {
+            secilenIndex = index;
+          });
+        },
 
-
-
-                             
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  IconButton(onPressed: (){
-                             
-                                  }, icon:Icon(Icons.favorite_border_sharp,size: 28,) ),
-                                  Image.network("http://kasimadalan.pe.hu/yemekler/resimler/${food.yemek_resim_adi}"),
-                                  Center(child: Text("${food.yemek_adi}",style: TextStyle(fontSize: 20),)),
-                                  const Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                    Icon(Icons.motorcycle),
-                                    Text("ücretsiz gönderim")
-                                  ],),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Text("${food.yemek_fiyat} "),
-                                      ElevatedButton(onPressed: (){
-                                        //sepete Ekleme
-                                      }, child: Icon(Icons.add)
-                                      ,style: ButtonStyle(
-                                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)))
-                                        ),)
-                                    ],),
-                                  )
-                             
-                                ],
-                              ),
-                             
-                                                   ),
-                           ),
-                         );
-                  }),
-                );
-              }
-              else{
-                return Center(
-                  child: Text("yk"),
-                );
-              }
-            }),
-          ],
-        ),
       ),
     );
   }
